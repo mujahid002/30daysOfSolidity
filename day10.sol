b16466948fa73c021ba83cb18f0b8e9a61678d1a
Mujahid002 <mujahidshaik2002@gmail.com>
2023-06-14 13:56:42 +0530

    power

---
+++ day10.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day10 {
+    function power(uint x,uint y) public pure returns(uint){
+            return x**y;
+    }
+}
