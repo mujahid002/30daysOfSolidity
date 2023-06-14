8ff1879b9dd5f5ba0bf9158801dc68545ae34f1d
Mujahid002 <mujahidshaik2002@gmail.com>
2023-06-14 14:00:34 +0530

    power

--- day_10.sol
+++ day_10.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Day10 {
-    function power(uint256256 x,uint256 y) public pure returns(uint256){
+    function power(uint256 x,uint256 y) public pure returns(uint256){
             return x**y;
     }
 }
